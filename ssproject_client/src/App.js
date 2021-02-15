bc84c1b69ba63e12d457fbfda90b5eaac467aa46
JeongSu <31645714+ParkJeongSu@users.noreply.github.com>
2021-02-15 18:56:55 +0900

    골격 구성

---
+++ ssproject_client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import MainView from './components/viewMain/MainView';
+function App() {
+  return (
+    <div className="App">
+      <MainView></MainView>
+    </div>
+  );
+}
+
+export default App;
